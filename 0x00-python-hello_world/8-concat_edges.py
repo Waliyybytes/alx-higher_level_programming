b53fc187cdf2c74a045d1b609416fe07c9acf2f8
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-23 15:48:27 +0100

    concat edges

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str4 = str4[39:66] + str4[106:111] + str4[6] + str4[0:6]
+print(str)
