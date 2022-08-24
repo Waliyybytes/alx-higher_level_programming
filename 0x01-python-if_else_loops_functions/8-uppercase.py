8aac25216305b757777e39b04d93b1f009135e0d
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-24 18:19:39 +0100

    print Uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new =''
+    for i in str:
+        if ord(i) >= 97 and (ord(i) <= 122):
+            new += chr(ord(i) - 32)
+        else:
+            new += i
+    print(new)
