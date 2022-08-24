00662f6cb766485106513917d227a3c9f8edef26
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-24 19:49:45 +0100

    FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for j in range(1, 100):
+    for j in range(1, 101):
         if j % 3 == 0 and j % 5 != 0:
-            print("fizz", end=" ")
+            print("Fizz", end=" ")
         elif j % 5 == 0 and j % 3 != 0:
-            print("buzz", end=" ")
+            print("Buzz", end=" ")
         elif j % 15 == 0:
-            print("fizzbuzz", end=" ")
+            print("FizzBuzz", end=" ")
         else:
             print("{}".format(j), end=" ")
