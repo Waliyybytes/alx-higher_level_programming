a18706efa1eb9017a6f5f2e5f11e7d818368ded8
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-25 02:26:14 +0100

    reverse_alpha_alternate

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def rev_alpha():
-    for j in range(122, 96, -1):
-        if j % 2 == 1:
-            j -= 32
-        print("{}".format(chr(j)), end='')
+for j in range(122, 96, -1):
+    if j % 2 == 1:
+        j -= 32
+    print("{}".format(chr(j)), end='')
