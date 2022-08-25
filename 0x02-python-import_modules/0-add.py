d220479dcc6d3b92ff8d4a713a60c9082f77606b
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-25 23:47:37 +0100

    import add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 from add_0 import add
 
+
 def addition():
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
+
 
 if __name == "__main":
     addition()
