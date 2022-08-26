ec90ab6ee14f822ec066b1c28ecb52f2f28ea584
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-26 17:33:07 +0100

    my_calc_device

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,22 +3,24 @@ from calculator_1 import add, sub, mul, div
 from sys import argv
 
 
+import sys
 def my_calculator():
-    a = argv[1]
-    b = argv[3]
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    match argv[2]:
-        case "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        case "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        case "*":
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        case "/":
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        match argv[2]:
+            case '+':
+                print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+            case '-':
+                print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+            case '*':
+                print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+            case '/':
+                print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+            case _:
+                print("Unknown operator. Available operators: +, -, * and /")
 
 
 if __name__ == "__main__":
