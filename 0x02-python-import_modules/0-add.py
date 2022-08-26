bd7021b99d060c03ac610bd1410c4b261207db69
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-26 03:11:36 +0100

    import additions bug

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,7 +5,7 @@ from add_0 import add
 def addition():
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
 
 if __name__ == "__main__":
