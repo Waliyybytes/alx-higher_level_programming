091a2e0898c34bbabee026ca833f12f0b2777b62
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-26 05:06:41 +0100

    infinite_add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def command_line():
+    add = 0
+    for i in sys.argv[1:]:
+        add += int(i)
+    print(add)
+
+if __name__ == "__main__":
+    command_line()
