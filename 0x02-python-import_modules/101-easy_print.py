614c98377c4155871968f51d9a6d488d4d71ac9e
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-26 18:45:47 +0100

    easy print

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,11 +1,2 @@
 #!/usr/bin/python3
-import os
-
-def easy_print():
-    st = "#pythoniscool\n"
-    line = str.encode(st)
-    os.write(1, line)
-
-
-if __name__ == "__main__":
-    easy_print()
+import os; line = str.encode("#pythoniscool\n"); os.write(1,line)
