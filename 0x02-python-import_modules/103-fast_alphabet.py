2c3b729018173d877e20cf0449620d6c2e4eb8b9
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-27 01:28:50 +0100

    fast alphabets

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from upper_alphabets import ascii_upper
+ascii_upper()
---
+++ 0x02-python-import_modules/upper_alphabets.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def ascii_upper():
+    for j in range(65, 91):
+        print("{}".format(chr(j)), end='')
+    print()
