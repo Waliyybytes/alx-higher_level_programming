08246296f44b78dab0c6d7e69c73c0a2d675f546
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-29 15:03:14 +0100

    NO C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new = []
+    if not my_string:
+        return
     for s in my_string:
-        if s != "c":
+        if s != 'c':
             new.append(s)
     return ''.join(new)
