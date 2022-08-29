13c98f8da6cfebf8039c16f62dc9468d362f1dcc
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-29 12:01:50 +0100

    data structures

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+	for num in my_list:
+		print("{}".format(num))
+
+if __name__ == "__main__":
+	print_list_integer()
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,5 @@
+This is the beginning of data structures in Python
+
+Python is cool
+
+
