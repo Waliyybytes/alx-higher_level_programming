30645a62cdbb4b629183fc9071bb5fbf3933d94b
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-29 15:43:42 +0100

    matrix print

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+def print_matrix_integer(matrix=[[]]):
+    for rows in matrix:
+        for i in rows:
+            if rows.index(i) != len(rows) - 1:
+                print("{:d}".format(i), end=' ')
+            else:
+                print("{:d}".format(i))
+    print('--')
+    print()
