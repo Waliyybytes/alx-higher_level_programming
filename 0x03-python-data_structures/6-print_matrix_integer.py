a6864d2461be8722c28cc3e5edfda51b26f1e3a1
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-29 17:24:08 +0100

    matrix print

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return
+    if matrix == [[]]:
+        print()
     for rows in matrix:
         for i in rows:
             if rows.index(i) != len(rows) - 1:
--- 0x03-python-data_structures/main_6.py
+++ 0x03-python-data_structures/main_6.py
@@ -7,6 +7,6 @@ matrix = [
     [7, 8, 9]
 ]
 
-print_matrix_integer(matrix)
+#print_matrix_integer(matrix)
 #print("--")
-#print_matrix_integer()
+print_matrix_integer()
