1dd5cff14d7802613002c56286d0d5a9cbe9985a
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-29 12:05:13 +0100

    list_structures

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-	for num in my_list:
-		print("{}".format(num))
+    for num in my_list:
+        print("{}".format(num))
+
 
 if __name__ == "__main__":
-	print_list_integer()
+    print_list_integer()
