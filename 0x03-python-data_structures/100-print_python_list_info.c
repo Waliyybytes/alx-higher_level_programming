a1f0a56d58f39141c7bc00769a6b842259062e69
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-30 02:05:09 +0100

    Cpython beginings

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,30 @@
+#include "Python.h"
+
+/**
+ * print_python_list_info - print info on python list
+ * @p: PyObject
+ * Return: Nothing
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	PyListObject *list;
+	int size, i = 0;
+	PyOject *obj;
+
+	list = (PyListObject *)p;
+	size = PyList_Size(p);
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	while (i < size)
+	{
+		obj = list->ob_item[i];
+		type = obj->ob_type;
+		printf("Element %d: %s\n", i, type->tp_name);
+		i++;
+	}
+
+	
+}
+
