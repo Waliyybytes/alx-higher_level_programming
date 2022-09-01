c0ffc389b16ac56a009e68154bfa5c8888813ddf
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-01 01:23:04 +0100

    unique add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,4 @@ def uniq_add(my_list=[]):
     for num in sorted(my_list):
         if my_list.count(num) > 1:
             my_list.remove(num)
-        else:
-            total += num
-    return total
+    return sum(my_list)
