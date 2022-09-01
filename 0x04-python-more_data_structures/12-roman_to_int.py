a126c0d52a04ba6785ea5d416d7ed97b4a2d9420
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-01 12:26:18 +0100

    roman_int_mapping

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
+    if type(roman_string) != str or roman_string == "":
+        return 0
+    i = 0
+    num = 0
+    while i < len(roman_string) - 1:
+        if roman[roman_string[i + 1]] <= roman[roman_string[i]]:
+            num += roman[roman_string[i]]
+        if roman[roman_string[i + 1]] > roman[roman_string[i]]:
+            num -= roman[roman_string[i]]
+        i += 1
+    num += roman[roman_string[i]]
+    return num
