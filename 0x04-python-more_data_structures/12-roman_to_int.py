b65453df593f597dfe6c52c91c025f11340d75ce
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-01 12:29:11 +0100

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     if type(roman_string) != str or roman_string == "":
         return 0
     i = 0
