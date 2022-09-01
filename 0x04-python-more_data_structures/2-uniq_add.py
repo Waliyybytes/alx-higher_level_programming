63e0f321a045dd2acdf0fdaa8eb72a1a7769b1b7
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-01 01:18:30 +0100

    uniques addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    for num in sorted(matrix):
-        if matrix.count(num) > 1:
-            matrix.remove(num)
+    for num in sorted(my_list):
+        if my_list.count(num) > 1:
+            my_list.remove(num)
         else:
             total += num
     return total
