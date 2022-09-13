028b3b6a460975675c66e7a7712cfeb3912e9460
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-13 17:44:29 +0100

    debug  task 2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         bul = True
-    except ValueError:
+    except (ValueError, TypeError):
         bul = False
     return bul
