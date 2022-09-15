4495c7bebff6ac448be508c3baab0675746a43a0
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-15 10:24:13 +0100

    task_2 solution

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" defining a class Square"""
+
+
+class Square:
+    """ class Square with
+
+    Attribute __size(int) set to private
+    It's crucial for a object like square
+
+    """
+    def __init__(self, size):
+        """ Initializes an instance
+
+        Args: size
+
+        """
+        self.__size = size
