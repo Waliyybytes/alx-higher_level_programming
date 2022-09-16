351df9f400d87eef91fe6e38a83c4c4f9a6b6268
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-16 18:04:33 +0100

    compare instances

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+""" defining a class Square"""
+
+
+class Square:
+    """
+    Square Blueprint
+    Attributes:
+        __size(int): size of side of a sqaure
+    Methods:
+        __init__
+        area
+    Property:
+        size: to retrieve it and set it
+
+    """
+    def __init__(self, size=0):
+        """
+        Initializes the square
+        Args:
+            size(int)
+        Returns:
+            None
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Evaluates the Area of the square
+        Args:
+            None
+        Returns:
+            result of area calculated (int)
+        """
+        return self.__size ** 2
+
+    def __lt__(self, other):
+        """compares two instances"""
+        return self.area() < other.area()
+
+    def __eq__(self, other):
+        """compares two instances"""
+        return self.area() == other.area()
+
+    def __le__(self, other):
+        """compares two instances"""
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """compares two instances"""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """compares two instances"""
+        return self.area() >= other.area()
+
+    def __ne__(self, other):
+        """compares two instances"""
+        return self.area() != other.area()
