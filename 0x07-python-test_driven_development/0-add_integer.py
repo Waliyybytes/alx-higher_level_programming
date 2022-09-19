07b89f5fbbf412b6266b1092e2a8a45dd4091192
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-19 13:57:23 +0100

    Test_driven_dev task 1

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Contains function to test
+
+    Function:
+        add_integer
+    
+"""
+
+
+def add_integer(a, b=98):
+    """ A function that returns addition of two numbers
+         Making sure a and b are integers
+        
+        Arguments:
+            a, b 
+    """
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    a, b = int(a), int(b)
+    return a + b
