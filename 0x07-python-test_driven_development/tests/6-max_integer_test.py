038439fa39cc8aba4bea36eedfe69e868f2bf4d4
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-22 18:46:22 +0100

    bugs cleaning

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+	Unittest for max_integer([..])
+"""
 import unittest
 max_integer = __import__("6-max_integer").max_integer
 
@@ -12,3 +15,6 @@ class TestMaxInteger(unittest.TestCase):
     def test_types(self):
         """ Tests the values in the list"""
         self.assertRaises(TypeError, max_integer, [1, -5, 1/7, False])
+        
+if __name__ == "__main__":
+    unittest.main()
