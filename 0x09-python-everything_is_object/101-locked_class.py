ac1a75bea5f44aea91b3b07cb01196dd88483bab
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-22 04:10:31 +0100

    adv_task_2

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 class LockedClass:
-    """ To dynamically prevent creation of other instance attributes 
+    """
+        To dynamically prevent creation of other instance attributes
 
         Except first_name
     """
+
     __slots__ = ['first_name']
