2d7cc5b893a3786820cd249c36876311f03132cd
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-26 22:31:46 +0100

    task_0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    To print all the methods and attributes of a class
+
+"""
+
+
+def lookup(obj):
+    """ method definition to return all class funtions
+        and attributes
+
+    """
+    return dir(obj)
