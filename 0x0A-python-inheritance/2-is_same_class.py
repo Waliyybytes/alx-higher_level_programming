8f6434fe4f1fbcb94e76b5c0d20a3394212e4c8c
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-26 22:42:06 +0100

    task_2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    To check the class instantiation
+
+"""
+
+
+def is_same_class(obj, a_class):
+    """ checks  if obj is an instance of class"""
+    return isintance(obj, a_class)
