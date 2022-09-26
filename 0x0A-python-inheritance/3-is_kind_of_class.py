60bc44264cba34c3cbb973ea388496bbd3f9fc59
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-26 23:08:11 +0100

    task3_solution

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    To check the class instantiation and inheritance
+
+"""
+
+
+def is_same_class(obj, a_class):
+    """ checks  if obj is an instance and parent of obj"""
+    return isinstance(obj, a_class)
