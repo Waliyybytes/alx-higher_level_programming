b5f19b3626c79dcafadc295edbc554a31938c20a
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-27 20:31:47 +0100

    task_13

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,7 +6,7 @@
 
 def add_attribute(obj, name, value):
     """ method definition to check attribute addition"""
-    if '__dict__' in dir(obj):
+    if '__dict__' in dir(obj) and isinstance(type(obj), object):
         obj.name = value
     else:
         raise TypeError("can't add new attribute")
