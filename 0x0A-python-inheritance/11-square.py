979ce91c96570198bab63af180e29a26803700b3
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-27 17:45:12 +0100

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Base Geometry module creation with subclass Rectangle
+
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+        Rectangle class inherits from BaseGeometry
+    """
+    def __init__(self, size):
+        """ attributes instantiation """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ evaluates the area of a square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Informal representation of a Square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
