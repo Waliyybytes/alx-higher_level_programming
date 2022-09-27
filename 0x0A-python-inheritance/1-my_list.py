53fe124e58f98468f93f024fb16f88f92aa9a3a1
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-27 12:10:10 +0100

    task_list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,13 +6,13 @@
 
 
 class MyList(list):
-    """ 
-        Class to demonstrate inheritance 
+    """
+        Class to demonstrate inheritance
         Inherits from class list
 
     """
     def print_sorted(self):
-        """ 
+        """
             method to demonstrate inheritance
 
         """
