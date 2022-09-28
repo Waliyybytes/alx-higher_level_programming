e7e5f9111dcd4413a509da8b9f37894f917fb876
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-28 19:42:49 +0100

    task_10 solution

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Class module to json
+"""
+
+
+class Student:
+    """ Class definition with methods '__init__' and 'to_json'"""
+    def __init__(self, first_name, last_name, age):
+        """ class instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Returns the dictionary description with
+                simple data structure
+            (list, dictionary, string, integer and boolean)
+            for JSON serialization of an object
+        """
+        get_attrs = {}
+        if attrs is not None:
+            for key in self.__dict__:
+                if key in attrs:
+                    get_attrs[key] = self.__dict__[key]
+            return get_attrs
+        return self.__dict__
