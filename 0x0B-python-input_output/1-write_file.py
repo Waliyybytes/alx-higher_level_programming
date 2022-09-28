67261fe30f48ce7ef149416bb30e7438361c21e7
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-28 13:07:18 +0100

    task_1 solution

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,5 @@
 
 def write_file(filename="", text=""):
     """ method definitiion to write to a file"""
-    with open(filename, mode = 'w', encoding="utf-8") as f:
+    with open(filename, mode='w', encoding="utf-8") as f:
         return f.write(text)
