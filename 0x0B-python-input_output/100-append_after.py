85f1cf9d20f927dff18c43665e0cc3b327abfad2
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-29 00:51:04 +0100

    task 13 solution

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Search and update a file
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ functions to search and update"""
+    with open(filename) as f:
+        fcopy = f.readlines()
+        ls = len(fcopy)
+    for i in range(ls):
+        if search_string in fcopy[i]:
+            fcopy = fcopy[:i+1] + [new_string] + fcopy[i+1:]
+            i += 1
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write("{}".format("".join(fcopy)))
