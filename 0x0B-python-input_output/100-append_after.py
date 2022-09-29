2b060ec55557bbc5968c21a7aa21db2ff899f574
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-29 01:26:19 +0100

    task_13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,12 +6,12 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """ functions to search and update"""
-    with open(filename) as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         fcopy = f.readlines()
         ls = len(fcopy)
-    for i in range(ls):
-        if search_string in fcopy[i]:
-            fcopy = fcopy[:i+1] + [new_string] + fcopy[i+1:]
-            i += 1
+        for i in range(ls):
+            if search_string in fcopy[i]:
+                fcopy = fcopy[:i+1] + [new_string] + fcopy[i+1:]
+                i += 1
     with open(filename, 'w', encoding="utf-8") as f:
         f.write("{}".format("".join(fcopy)))
