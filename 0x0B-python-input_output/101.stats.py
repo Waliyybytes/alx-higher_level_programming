34d3ba7414bed7015eabb9fbc9e87b36045653ee
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-30 13:03:10 +0100

    task 13 solution

---
+++ 0x0B-python-input_output/101.stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys
+
+s_code = {'200': 0, '301': 0, '400': 0, '401': 0,
+          '403': 0, '404': 0, '405': 0, '500': 0}
+f_size = 0
+tens = 0
+try:
+    for line in sys.stdin:
+        tens += 1
+        f_size += int(line.split()[-1])
+        s_code[line.split()[-2]] += 1
+        if tens == 10:
+            print("File size: {}".format(f_size))
+            for key in s_code:
+                print("{}: {}".format(key, s_code[key]))
+            tens = 0
+except KeyboardInterrupt as e:
+    print(e)
