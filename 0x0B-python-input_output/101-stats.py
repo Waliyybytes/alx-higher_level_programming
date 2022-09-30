5684e212383f0f6e14f38da78ff6b34a9188aacd
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-30 15:31:17 +0100

    task 13 advanced

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -11,16 +11,17 @@ f_size = 0
 tens = 0
 try:
     for line in sys.stdin:
-        f_size += int(line.split()[-1])
-        if line.split()[-2] in s_code:
-            s_code[line.split()[-2]] += 1
-        tens += 1
+        if len(line.split()) >= 2:
+            f_size += int(line.split()[-1])
+            if line.split()[-2] in s_code:
+                s_code[line.split()[-2]] += 1
+            tens += 1
         if tens % 10 == 0:
             print("File size: {}".format(f_size))
             for key in s_code:
                 if s_code[key]:
                     print("{}: {}".format(key, s_code[key]))
-except KeyboardInterrupt as e:
+except KeyboardInterrupt:
     print("File size: {}".format(f_size))
     for key in s_code:
         if s_code[key]:
