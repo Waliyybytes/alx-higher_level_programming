ff7b3d05789cd54bbf02b0f6f1aafb2ed07f1497
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-10-05 09:37:21 +0100

    over-indented bug

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,8 +79,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """ informal representation of Rectangle class"""
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - " +
-    "{self.__width}/{self.__height}"
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - " + \
+            "{self.__width}/{self.__height}"
 
     def update(self, *args, **kwargs):
         """ update arguments via args"""
