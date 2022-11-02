68012266f556f34dd8412785a42bed109ab81cd7
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-02 15:35:00 +0100

    task_12

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,7 +1,5 @@
 -- script that lists all records of the table second_table 
-UPDATE 
-(SELECT score, name 
+UPDATE second_table SET score = 10 WHERE name = 'BOB';
+SELECT score, name 
 FROM second_table
-ORDER BY score DESC)
-SET score = 10
-WHERE name = 'Bob';
+ORDER BY score DESC;
