ba639118517a8b79550505fddac268a1cc662a3e
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-03 23:59:09 +0100

    task_12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,6 +3,6 @@ SELECT x.title, y.genre_id
 FROM tv_shows x
 LEFT JOIN tv_show_genres y
 ON x.id = y.show_id
-WHERE y.genre_id = NULL
+WHERE y.genre_id IS NULL
 ORDER BY x.title ASC, y.genre_id ASC;
 
