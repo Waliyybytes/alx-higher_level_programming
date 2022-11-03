0c51b8b311e2030a456fe1af80a054fa017db77f
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-03 23:14:43 +0100

    task_9 solution

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- script that lists all the cities of California that can be found in the database
+SELECT cities.id AS id, cities.name AS NAME, states.name AS name
+FROM cities
+LEFT JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id;
