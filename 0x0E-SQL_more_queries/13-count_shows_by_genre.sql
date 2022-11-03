48adc25e1813f5e1597a114503afc84c03d43b35
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-04 00:24:15 +0100

    tASk_12

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- script that lists all shows contained in hbtn_0d_tvshows
-SELECT x.genre_id AS genre, COUNT(y.title) AS number_of_shows
-FROM tv_show_genres x
-INNER JOIN tv_shows y
-ON x.show_id = y.id
+SELECT x.name AS genre, COUNT(y.genre_id) AS number_of_shows
+FROM tv_genres x
+INNER JOIN tv_show_genres y
+ON x.id = y.genre_id
 GROUP BY x.genre_id
 ORDER BY number_of_shows DESC;
 
