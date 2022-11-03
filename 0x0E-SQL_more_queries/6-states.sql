d33fb3dac5227c87429d884028108cff5cbfdd55
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-03 22:06:48 +0100

    task_6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- script that creates database and table in a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
