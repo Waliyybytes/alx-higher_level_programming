0e8893b36a2976d406dbe2e992c67902ea825fc1
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-04 00:33:50 +0100

    task_14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- script that lists all shows contained in hbtn_0d_tvshows
+SELECT x.name
+FROM tv_genres x
+INNER JOIN tv_show_genres y
+ON x.id = y.genre_id
+INNER JOIN tv_shows z
+ON y.show_id = z.id
+WHERE z.title = 'Dexter';
+
