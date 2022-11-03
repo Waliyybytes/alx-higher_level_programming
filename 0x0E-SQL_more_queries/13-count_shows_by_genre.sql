ad6bbbfab9735f31834676350738ef9904e098eb
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-04 00:25:27 +0100

    tasl_13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,6 +3,6 @@ SELECT x.name AS genre, COUNT(y.genre_id) AS number_of_shows
 FROM tv_genres x
 INNER JOIN tv_show_genres y
 ON x.id = y.genre_id
-GROUP BY x.genre_id
+GROUP BY y.genre_id
 ORDER BY number_of_shows DESC;
 
