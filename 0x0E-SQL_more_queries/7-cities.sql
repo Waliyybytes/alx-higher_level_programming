b4ffd2a23df4808efbe0cf97fcb824bc21c7c430
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-03 22:57:06 +0100

    task_7 soln

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,8 +2,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-	state_id INT NOT NULL
+	state_id INT NOT NULL,
 	FOREIGN KEY (state_id)
-	    REFERENCES hbtn_0d_usa.states(id),
+       		REFERENCES hbtn_0d_usa.states(id),
 	name VARCHAR(256) NOT NULL
 );
