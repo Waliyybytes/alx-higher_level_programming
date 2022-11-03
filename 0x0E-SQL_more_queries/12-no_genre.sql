0732421f7bf0852ef50d5f1ed16dd1791c5bc6a0
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-03 23:43:17 +0100

    task_12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- script that lists all shows contained in hbtn_0d_tvshows
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE genre_id = NULL
+ORDER BY title, genre_id;
+
