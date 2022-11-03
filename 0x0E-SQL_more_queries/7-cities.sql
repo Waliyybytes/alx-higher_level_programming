1c8cfbafaa1138be63441938865d1b7d9de5007f
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-03 22:50:26 +0100

    task_7 soln

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 -- script that creates database and table in a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
 	FOREIGN KEY (state_id)
-	    REFERENCES state(id),
+	    REFERENCES states(id),
 	name VARCHAR(256) NOT NULL
 );
