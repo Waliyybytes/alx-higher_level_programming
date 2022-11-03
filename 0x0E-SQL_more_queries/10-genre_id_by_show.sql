cb8254460758362ade23d98a6c213bb0ba15f717
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-03 23:34:19 +0100

    task_10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT tv_shows.title AS title, tv_show_genres.genre.id AS genre_id
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-ORDER BY title, genre_id;
+ORDER BY tv_shows.title,tv_show_genres.genre_id;
 
