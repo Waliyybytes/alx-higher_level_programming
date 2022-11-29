8094dec233985f687a68dfd1d0fdf75ff3d53045
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-29 04:39:20 +0100

    task8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+if (num) {
+  for (let i = 0; i < num; i++) console.log('X'.repeat(num));
+} else {
+  console.log('Missing number of occurrences');
+}
