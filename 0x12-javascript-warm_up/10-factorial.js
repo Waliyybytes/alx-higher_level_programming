8992fc7ad838a6ebeaff196903d2e721e8945efb
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-29 15:01:43 +0100

    task_10 bugs fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-function factorial(n) {
-    return (n ? n * factorial(n-1) : 1);
+function factorial (n) {
+  return (n ? n * factorial(n - 1) : 1);
 }
 
 console.log(factorial(parseInt(process.argv[2])));
