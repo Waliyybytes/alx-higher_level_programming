51ac75168ee44a417c8c21887af130b7eb069a73
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-29 04:26:43 +0100

    tasks

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+num ? (function (num) { for (let i = 0; i < num; i++) console.log('C is fun'); })(num) : console.log('Missing number of occurrences');
