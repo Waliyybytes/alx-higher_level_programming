58442c5b7e819147a1d8b06a301e829dac8cc491
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-12-05 22:37:01 +0100

    some tasks

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""lists State objects from the database hbtn_0e_6_usa that contains a"""
+from model_state import Base, State
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3]
+        )
+    )
+    session = sessionmaker(bind=engine)()
+    nu = State(name='Louisana')
+    session.add(nu)
+    session.commit()
+    print(nu.id)
