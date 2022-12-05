fe9fda23ef131770fd5d264cbad018bd4da58a05
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-12-05 23:18:21 +0100

    tasks

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""lists all city objects from the database hbtn_0e_6_usa
+"""
+from model_city import Base, City
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3]
+        )
+    )
+    session = sessionmaker(bind=engine)()
+    for c_id, c_name, s_name in session.query(City.id, City.name, State.name).order_by(City.id):
+        print("{}: ({}) {}".format(s_name, c_id, c_name))
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Declaring city Model """
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import State
+
+Base = declarative_base()
+
+
+class City(Base):
+    """ city class """
+    __tablename__ = 'cities'
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    name = Column("name", String(128), nullable=False)
+    state_id = Column("state_id", Integer, ForeignKey(State.id))
