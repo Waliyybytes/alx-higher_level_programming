625c17f8c0b7e017bfd1d677038b4fc499993426
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-12-06 04:26:21 +0100

    all_tasks

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,8 +2,7 @@
 """ Declaring city Model """
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base,State
-
+from model_state import Base, State
 
 
 class City(Base):
