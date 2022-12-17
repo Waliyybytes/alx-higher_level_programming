7934b540932fb21734a0a29ae67e10fa64e1eceb
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-12-17 02:29:27 +0100

    checks

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,6 +8,10 @@ def find_peak(list_of_integers):
     if arr:
         if len(arr) == 1:
             return arr[0]
+        if arr[0] > arr[1]:
+            return arr[0]
+        if arr[len(arr) - 1] > arr[len(arr) - 2]:
+            return arr[len(arr) - 1]
         mid = len(arr) // 2
         if arr[mid - 1] >= arr[mid]:
             return find_peak(arr[0:mid])
