fd37238a1f686e4c3b1b64ad6f95cb02c7c808e8
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-12-17 02:32:58 +0100

    checks

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request
-curl -s -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
+curl -s -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
