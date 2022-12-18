5cb518a3efde6a9a9431600f73d79088dd5ad807
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-12-18 22:09:50 +0100

    task-7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""print error code on error
+"""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    r = requests.get(argv[1])
+    if r.status_code >= 400:
+        print(f'Error code: {r.status_code}')
+    else:
+        print(r.text)
