352401e90036fd51bcf711d5ffbbbdf77e34ba4d
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-12-18 21:58:30 +0100

    task debug

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         result = response.read()
         print('Body response:')
-        print('\t- type: {}'.format(type(result)))
-        print('\t- content: {}'.format(result))
+        print(f"\t- type: {type(result)}")
+        print(f"\t- content: {result}")
         print('\t- utf8 content: {}'.format(result.decode('utf-8')))
