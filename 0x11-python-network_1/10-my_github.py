61b0daa3788b182153478ac6c4f62affc4243438
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-12-18 23:09:57 +0100

    task_9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,5 +8,5 @@ from requests.auth import HTTPBasicAuth
 if __name__ == '__main__':
     checks = HTTPBasicAuth(argv[1], argv[2])
     r = requests.get('https://api.github.com/user', auth=checks)
-    j_obj= r.json()
+    j_obj = r.json()
     print(j_obj.get('id'))
